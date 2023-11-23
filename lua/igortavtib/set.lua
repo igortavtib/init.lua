5bf40fa3596740f9640c4da22af9e3c536596982
igor.tavares <igor.tavares@zenvia.com>
2023-11-23 12:17:05 -0300

    feat(set): update tab settings

--- lua/igortavtib/set.lua
+++ lua/igortavtib/set.lua
@@ -1,6 +1,11 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 vim.opt.scrolloff = 7
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
